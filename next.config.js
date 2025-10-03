870c1250baaf53b1333c2a206ecb9c3ab20b06b1
Prince <techtune.it.solutions@gmail.com>
2025-10-03 06:16:26 -0500

    corrected surprise

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vkftywhuaxwbknlrymnr.supabase.co',
+        pathname: '/storage/v1/object/public/media/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig;
