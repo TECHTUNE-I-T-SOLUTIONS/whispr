e0aa0c7f27a815605e8a7cb1ef4a802bf33f3f29
Prince <techtune.it.solutions@gmail.com>
2026-01-03 22:10:26 -0500

    feat: autoscroll and voice reader

--- components/comments.tsx
+++ components/comments.tsx
@@ -14,6 +14,7 @@ import { MessageCircle, Send, Heart } from "lucide-react"
 import { useToast } from "@/hooks/use-toast"
 import { useCommentCache } from "@/hooks/use-comment-cache"
 
+// Comment type definition
 interface Comment {
   id: string
   content: string
