ebf0df3ccc9c19a34311ff51f9b46d28f906c8a2
Prince <techtune.it.solutions@gmail.com>
2026-01-03 22:07:38 -0500

    another

--- components/poem-lyrics-view.tsx
+++ components/poem-lyrics-view.tsx
@@ -1,5 +1,6 @@
 // This file is deprecated - auto-scroll functionality moved to advanced-features-modal.tsx
 // and integrated directly into poem/blog content cards for in-place scrolling
+// this isn't used anymore but kept for reference
 
 export function PoemLyricsView() {
   return null
